1a13f1a4032a16528f59415235cc299ae45e317f
Safvan-tsy <safvanmanikulath@gmail.com>
2023-10-01 16:04:47 +0530

    product add form

--- src/components/products/product-form/ProductAdd.tsx
+++ src/components/products/product-form/ProductAdd.tsx
@@ -1,12 +1,13 @@
 import { FormControlLabel, Radio, RadioGroup, TextField, FormLabel, ImageListItem, ImageList } from '@mui/material';
 import './product-form.css'
 import UploadSvg from "./Upload.svg?react";
+
 const ProductAdd = () => {
 
   const itemData = [
-    {img:"https://res.cloudinary.com/unfoldcloud/image/upload/v1683805642/blogs/blog-63f2fa7a234c1ee0220836ff-1683805639721-cover.jpg",title:"title"},
-    {img:"https://res.cloudinary.com/unfoldcloud/image/upload/v1684737834/blogs/blog-63f2fa7a234c1ee0220836ff-1684737832201-cover.jpg",title:"title"},
-    {img:"https://res.cloudinary.com/unfoldcloud/image/upload/v1684737248/blogs/blog-63f2fa7a234c1ee0220836ff-1684737246148-cover.jpg",title:"title"},
+    { img: "https://res.cloudinary.com/unfoldcloud/image/upload/v1683805642/blogs/blog-63f2fa7a234c1ee0220836ff-1683805639721-cover.jpg", title: "title" },
+    { img: "https://res.cloudinary.com/unfoldcloud/image/upload/v1684737834/blogs/blog-63f2fa7a234c1ee0220836ff-1684737832201-cover.jpg", title: "title" },
+    { img: "https://res.cloudinary.com/unfoldcloud/image/upload/v1684737248/blogs/blog-63f2fa7a234c1ee0220836ff-1684737246148-cover.jpg", title: "title" },
   ]
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -91,18 +92,18 @@ const ProductAdd = () => {
             </div>
             <div className='product-form-field '>
               <div className='img-list'>
-              <ImageList variant="masonry" cols={3} gap={8}>
-                {itemData.map((item) => (
-                  <ImageListItem key={item.img}>
-                    <img
-                      srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      src={`${item.img}?w=248&fit=crop&auto=format`}
-                      alt={item.title}
-                      loading="lazy"
-                    />
-                  </ImageListItem>
-                ))}
-              </ImageList>
+                <ImageList variant="masonry" cols={3} gap={8}>
+                  {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                      <img
+                        srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item.img}?w=248&fit=crop&auto=format`}
+                        alt={item.title}
+                        loading="lazy"
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
               </div>
             </div>
           </div>
