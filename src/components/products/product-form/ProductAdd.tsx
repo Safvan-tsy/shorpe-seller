825f99476d8b489cebc80dd2b0b7f7b198a1f691
Safvan-tsy <safvanmanikulath@gmail.com>
2023-10-17 22:47:16 +0530

    dropdown list

--- src/components/products/product-form/ProductAdd.tsx
+++ src/components/products/product-form/ProductAdd.tsx
@@ -5,12 +5,54 @@ import {
   TextField,
   FormLabel,
   ImageListItem,
-  ImageList
+  ImageList,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+  ListItemText,
+  OutlinedInput,
+  SelectChangeEvent,
+  Checkbox
 } from '@mui/material';
 import './product-form.css'
 import UploadSvg from "./Upload.svg?react";
+import React from 'react';
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const districtlist = [
+  'Alappuzha',
+  'Eranakulam',
+  'Idukki',
+  'Kannur',
+  'Kasarkode',
+  'Kollam',
+  'Kottayam',
+  'Kozhikode',
+  'Malappuram',
+  'Palakkad',
+  'Pathanamthitta',
+  'Thiruvananthapuram',
+  'Thrishur',
+  'Wayanad'
+]
+const pincodelist = [
+  '673573 - parappanpoyil',
+]
 const ProductAdd = () => {
+  const [districts, setDistricts] = React.useState<string[]>([]);
+  const [pincodes, setPincodes] = React.useState<string[]>([]);
+
 
   const itemData = [
     { img: "https://res.cloudinary.com/unfoldcloud/image/upload/v1683805642/blogs/blog-63f2fa7a234c1ee0220836ff-1683805639721-cover.jpg", title: "title" },
@@ -19,6 +61,24 @@ const ProductAdd = () => {
   ]
   const submitHandler = async (e) => {
     e.preventDefault();
+  };
+
+  const handleDistrictChange = (event: SelectChangeEvent<typeof districts>) => {
+    const {
+      target: { value },
+    } = event;
+    setDistricts(
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handlepincodeChange = (event: SelectChangeEvent<typeof pincodes>) => {
+    const {
+      target: { value },
+    } = event;
+    setPincodes(
+      typeof value === 'string' ? value.split(',') : value,
+    );
   };
 
   return (
@@ -114,6 +174,59 @@ const ProductAdd = () => {
                 </ImageList>
               </div>
             </div>
+            <div className='product-form-field'>
+              <FormLabel id="demo-row-radio-buttons-group-label">Delivery Possibility</FormLabel>
+              <RadioGroup row name="row-radio-buttons-group">
+                <FormControlLabel value="state" control={<Radio />} label="State" />
+                <FormControlLabel value="district" control={<Radio />} label="Select Districts" />
+                <FormControlLabel value="pincodes" control={<Radio />} label="Select Pincodes" />
+              </RadioGroup>
+            </div>
+            <div className='product-form-field'>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-checkbox-label">District</InputLabel>
+                <Select
+                  labelId="demo-multiple-checkbox-label"
+                  id="demo-multiple-checkbox"
+                  multiple
+                  value={districts}
+                  onChange={handleDistrictChange}
+                  input={<OutlinedInput label="Tag" />}
+                  renderValue={(selected) => selected.join(', ')}
+                  MenuProps={MenuProps}
+                >
+                  {districtlist.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={districts.indexOf(name) > -1} />
+                      <ListItemText primary={name} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+            <div className='product-form-field'>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-checkbox-label">Pincodes</InputLabel>
+                <Select
+                  labelId="demo-multiple-checkbox-label"
+                  id="demo-multiple-checkbox"
+                  multiple
+                  value={pincodes}
+                  onChange={handlepincodeChange}
+                  input={<OutlinedInput label="Tag" />}
+                  renderValue={(selected) => selected.join(', ')}
+                  MenuProps={MenuProps}
+                >
+                  {pincodelist.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={pincodes.indexOf(name) > -1} />
+                      <ListItemText primary={name} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+
           </div>
           <div >
             <button className='btn'>Submit</button>
